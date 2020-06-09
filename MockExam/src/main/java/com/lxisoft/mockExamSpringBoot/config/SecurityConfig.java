f36d8c29815dbdb6ae824f54d58ec414572943c9
Adarsh <adarsh.m.a@lxisoft.com>
2020-06-09 22:43:31 +0530

    http://jira.bytatech.com:8080/browse/IN-6385

---
+++ MockExam/src/main/java/com/lxisoft/mockExamSpringBoot/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+//package com.lxisoft.mockExamSpringBoot.config;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//	
+//	@Autowired
+//	public void configureGlobal(AuthenticationManagerBuilder authenticationMgr) throws Exception {
+//		authenticationMgr.inMemoryAuthentication()
+//		.withUser("admin").password("{noop}pasword").authorities("ROLE_USER","ROLE_ADMIN")
+//		.and()
+//		.withUser("adarsh").password("{noop}123456").authorities("ROLE_USER");
+//	}
+//	
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception {
+//		http.authorizeRequests()
+//			.antMatchers("/").access("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN') ")
+//			.and()
+//				.formLogin().loginPage("/loginPage")
+//				.defaultSuccessUrl("/check")
+//				.failureUrl("/loginPage?error")
+//				.usernameParameter("username").passwordParameter("password")				
+//			.and()
+//				.logout().logoutSuccessUrl("/loginPage?logout"); 
+//		
+//	}
+//
+//}
--- MockExam/src/main/java/com/lxisoft/mockExamSpringBoot/controller/MockController.java
+++ MockExam/src/main/java/com/lxisoft/mockExamSpringBoot/controller/MockController.java
@@ -4,18 +4,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
-import java.util.TreeSet;
+//import java.util.Set;
+//import java.util.TreeSet;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.core.Authentication;
+//import org.springframework.security.core.context.SecurityContextHolder;
 
 import com.lxisoft.mockExamSpringBoot.entity.MockEntity;
 import com.lxisoft.mockExamSpringBoot.service.*;
@@ -40,11 +43,39 @@ public class MockController {
 		 return model;
 	 }
 	
-//	@GetMapping("/user")
-//	public String userView() {
-//		return "Introduction";
-//	}
-	
+//	 @GetMapping(value = "/loginPage")
+//		public ModelAndView loginPage(@RequestParam(value = "error",required = false) String error,
+//		@RequestParam(value = "logout",	required = false) String logout) {
+//			
+//			ModelAndView model = new ModelAndView();
+//			if (error != null) {
+//				model.addObject("error", "Invalid Credentials provided.");
+//			}
+//
+//			if (logout != null) {
+//				model.addObject("message", "Logged outsuccessfully.");
+//			}
+//
+//			model.setViewName("Login");
+//			return model;
+//		}
+	 
+//	 	@GetMapping(value = "/authentication")
+//	   	public String userAuthentication() {
+//
+//			Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+//			boolean hasRole = authentication.getAuthorities().stream()
+//			          .anyMatch(r -> r.getAuthority().equals("ROLE_ADMIN"));
+//			if(hasRole)
+//			{
+//				return "redirect:/testAdmin";
+//			}
+//			else
+//			{
+//				return "redirect:/user";
+//			}
+//	   	}
+	 
 	@GetMapping("/user")
 	public String setQuestionsSession(HttpServletRequest request) {
 		HttpSession sessions = request.getSession(true);
