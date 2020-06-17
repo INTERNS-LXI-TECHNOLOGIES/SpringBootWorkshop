b39ad8f4c1b6251059ce5bea97e4c478adbe50ec
rahulrjr <rahul.m.p@lxisoft.com>
2020-06-17 17:36:31 +0530

    http://jira.bytatech.com:8080/browse/IN-6650http://jira.bytatech.com:8080/browse/IN-6646http://jira.bytatech.com:8080/browse/IN-6647

--- MockExam/mockexam/src/main/java/com/lxisoft/mockexam/controller/MockExamController.java
+++ MockExam/mockexam/src/main/java/com/lxisoft/mockexam/controller/MockExamController.java
@@ -5,9 +5,14 @@ import com.lxisoft.mockexam.service.ExamService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.net.http.HttpRequest;
 import java.util.List;
 
 @Controller
@@ -39,5 +44,42 @@ public class MockExamController
     {
         examService.saveQuestion(exam);
         return new ModelAndView("redirect:/admin");
+    }
+    @RequestMapping(value = "/update/{id}")
+    public ModelAndView updateQuestion(@PathVariable("id") int qId)
+    {
+        ModelAndView model = new ModelAndView();
+        Exam question = examService.getQuestionById(qId);
+        model.addObject("questionById",question);
+        model.setViewName("update");
+        return model;
+    }
+
+    @RequestMapping(value = "/updateQuestion")
+    public ModelAndView update(@ModelAttribute Exam exam)
+    {
+        examService.saveQuestion(exam);
+        return new ModelAndView("redirect:/admin");
+    }
+
+    @RequestMapping(value = "/delete/{id}")
+    public ModelAndView deleteQuestion(@PathVariable("id") int qId)
+    {
+        examService.deleteQuestion(qId);
+        return new ModelAndView("redirect:/admin");
+    }
+
+    @RequestMapping(value = "/logout")
+    public String logout(HttpServletRequest request)
+    {
+        HttpSession session = request.getSession();
+        session.invalidate();
+        return "index";
+    }
+
+    @RequestMapping(value = "/introduction")
+    public String instruction()
+    {
+        return "instruction";
     }
 }
