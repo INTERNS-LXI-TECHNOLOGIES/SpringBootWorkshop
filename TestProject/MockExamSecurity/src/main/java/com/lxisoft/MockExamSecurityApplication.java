e5425ee9e0f57c232ff37977c70e1ffb92227b7f
megha-vt <megha.v.t@lxisoft.com>
2020-06-22 18:30:20 +0530

    http://jira.bytatech.com:8080/browse/IN-6799

---
+++ TestProject/MockExamSecurity/src/main/java/com/lxisoft/MockExamSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.lxisoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockExamSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockExamSecurityApplication.class, args);
+	}
+
+}
