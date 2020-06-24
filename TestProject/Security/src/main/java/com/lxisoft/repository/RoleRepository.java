d29cf6062ee4fb0bd2bd8efc64023ed213aebb6c
user <amritha.p.m@lxisoft>
2020-06-24 18:53:10 +0530

    http://jira.bytatech.com:8080/browse/IN-6887

---
+++ TestProject/Security/src/main/java/com/lxisoft/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.lxisoft.repository;
+
+import com.lxisoft.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long>{
+}
