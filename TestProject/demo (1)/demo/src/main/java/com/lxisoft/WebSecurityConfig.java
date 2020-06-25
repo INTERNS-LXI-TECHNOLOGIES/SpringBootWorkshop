1bdae8122921c75dba53e22ab12d15c68ff43cb8
megha-vt <megha.v.t@lxisoft.com>
2020-06-25 17:36:26 +0530

    http://jira.bytatech.com:8080/browse/IN-6916

--- TestProject/demo (1)/demo/src/main/java/com/lxisoft/WebSecurityConfig.java
+++ TestProject/demo (1)/demo/src/main/java/com/lxisoft/WebSecurityConfig.java
@@ -1,11 +1,10 @@
 package com.lxisoft;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -14,8 +13,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true, proxyTargetClass = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    @Qualifier("userDetailsServiceImpl")
     @Autowired
     private UserDetailsService userDetailsService;
 
@@ -28,20 +27,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-                .antMatchers("/resources/**", "/registration").permitAll()
-                .anyRequest().authenticated()
-                .and()
+            .antMatchers("/resources/**", "/WEB-INF/jsp/*").permitAll()
+            .anyRequest().authenticated()
+            .and()
             .formLogin()
-                .loginPage("/login")
-                .permitAll()
-                .and()
+            .loginPage("/login")
+            .defaultSuccessUrl("/welcome")
+            .failureUrl("/login?error")
+            .permitAll()
+            .and()
             .logout()
-                .permitAll();
-    }
-
-    @Bean
-    public AuthenticationManager customAuthenticationManager() throws Exception {
-        return authenticationManager();
+            .permitAll();
     }
 
     @Autowired
--- TestProject/demo (1)/demo/src/main/java/com/lxisoft/web/UserController.java
+++ TestProject/demo (1)/demo/src/main/java/com/lxisoft/web/UserController.java
@@ -1,49 +1,14 @@
 package com.lxisoft.web;
 
-import com.lxisoft.model.User;
-import com.lxisoft.service.SecurityService;
-import com.lxisoft.service.UserService;
-import com.lxisoft.validator.UserValidator;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class UserController {
-    @Autowired
-    private UserService userService;
 
-    @Autowired
-    private SecurityService securityService;
-
-    @Autowired
-    private UserValidator userValidator;
-
-    @GetMapping("/registration")
-    public String registration(Model model) {
-        model.addAttribute("userForm", new User());
-
-        return "registration";
-    }
-
-    @PostMapping("/registration")
-    public String registration(@ModelAttribute("userForm") User userForm, BindingResult bindingResult) {
-        userValidator.validate(userForm, bindingResult);
-
-        if (bindingResult.hasErrors()) {
-            return "registration";
-        }
-
-        userService.save(userForm);
-
-        securityService.autoLogin(userForm.getUsername(), userForm.getPasswordConfirm());
-
-        return "redirect:/welcome";
-    }
-
-    @GetMapping("/login")
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
     public String login(Model model, String error, String logout) {
         if (error != null)
             model.addAttribute("error", "Your username and password is invalid.");
@@ -54,8 +19,12 @@ public class UserController {
         return "login";
     }
 
-    @GetMapping({"/", "/welcome"})
+    @RequestMapping(value = {
+        "/",
+        "/welcome"
+    }, method = RequestMethod.GET)
     public String welcome(Model model) {
         return "welcome";
     }
 }
+
