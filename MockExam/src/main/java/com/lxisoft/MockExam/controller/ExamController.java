4da50f790478d052c844bd4bbb004098aad53817
user <amritha.p.m@lxisoft>
2020-07-02 12:36:05 +0530

    http://jira.bytatech.com:8080/browse/IN-7138

--- MockExam/src/main/java/com/lxisoft/MockExam/controller/ExamController.java
+++ MockExam/src/main/java/com/lxisoft/MockExam/controller/ExamController.java
@@ -5,11 +5,11 @@ package com.lxisoft.MockExam.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -48,7 +48,7 @@ public class ExamController {
 		examRepository.save(exam);
 		return "redirect:list";
 	}
-
+	
 	@GetMapping("edit/{id}")
 	public String showUpdateForm(@PathVariable("id") long id, Model model) {
 		Exam exam = examRepository.findById(id)
@@ -57,7 +57,7 @@ public class ExamController {
 		return "update-exam";
 	}
 
-	 @PatchMapping("update/{id}") public String updateExam(@PathVariable("id")
+	 @PostMapping("update/{id}") public String updateExam(@PathVariable("id")
 	 long id, Exam exam, BindingResult result, Model model) { if
 	 (result.hasErrors()) { exam.setId(id); return "update-exam"; }
 	 
