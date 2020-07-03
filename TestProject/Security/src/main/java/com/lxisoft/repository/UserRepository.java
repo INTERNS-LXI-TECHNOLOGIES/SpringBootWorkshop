2585847efdd7f2fce1086646df201f4822fa3d5a
user <amritha.p.m@lxisoft>
2020-07-03 18:02:11 +0530

    http://jira.bytatech.com:8080/browse/IN-7281

--- TestProject/Security/src/main/java/com/lxisoft/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.lxisoft.repository;
-
-import com.lxisoft.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
--- TestProject/Security/src/main/java/com/lxisoft/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.lxisoft.service;
-
-import com.lxisoft.model.User;
-
-public interface UserService {
-    void save(User user);
-
-    User findByUsername(String username);
-}
