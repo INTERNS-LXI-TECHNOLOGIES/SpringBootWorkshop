93033bfbdd413dab48cfe69715a91be0ba159051
sanfar <muhammed.sanfar.m@lxisoft.com>
2020-07-14 23:36:16 +0530

    http://jira.bytatech.com:8080/browse/IN-7952

--- MockExam/src/main/java/com/lxisoft/MockExam/service/QuestionService.java
+++ MockExam/src/main/java/com/lxisoft/MockExam/service/QuestionService.java
@@ -1,5 +1,7 @@
 package com.lxisoft.MockExam.service;
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,7 +19,7 @@ public class QuestionService {
 	            questionRepository.save(question);
 	    }
 	    
-	    public List<Question> findById(int id)
+	    public Optional<Question> findById (int id)
 	    {
 	        return  questionRepository.findById(id);
 	    }
