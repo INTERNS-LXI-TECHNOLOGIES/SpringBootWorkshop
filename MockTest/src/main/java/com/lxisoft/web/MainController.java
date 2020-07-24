1d5e97d8b658f6178907ad5608cb5b080bada37b
megha-vt <megha.v.t@lxisoft.com>
2020-07-24 19:01:59 +0530

    http://jira.bytatech.com:8080/browse/IN-8578

--- MockTest/src/main/java/com/lxisoft/web/MainController.java
+++ MockTest/src/main/java/com/lxisoft/web/MainController.java
@@ -3,6 +3,11 @@ package com.lxisoft.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.lxisoft.model.Exam;
 
 @Controller
 public class MainController {
@@ -20,5 +25,13 @@ public class MainController {
     @GetMapping("/user")
     public String userIndex() {
         return "user/index";
+    }
+    
+    @RequestMapping(value = "/newquestion", method = RequestMethod.GET)
+    public ModelAndView newContact(ModelAndView model) {
+        Exam exam = new Exam();
+        model.addObject("exam", exam);
+        model.setViewName("add");
+        return model;
     }
 }
