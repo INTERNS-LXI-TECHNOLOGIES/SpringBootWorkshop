e808e6893a7550f16210a859bb08c1023105ea8f
megha-vt <megha.v.t@lxisoft.com>
2020-07-25 16:49:51 +0530

    http://jira.bytatech.com:8080/browse/IN-8630

--- MockTest/src/main/java/com/lxisoft/web/MainController.java
+++ MockTest/src/main/java/com/lxisoft/web/MainController.java
@@ -1,17 +1,28 @@
 package com.lxisoft.web;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.lxisoft.entity.Answer;
+import com.lxisoft.entity.QnOption;
+import com.lxisoft.entity.Question;
 import com.lxisoft.model.Exam;
+import com.lxisoft.repository.QuestionRepository;
 
 @Controller
 public class MainController {
-
+	@Autowired
+    private QuestionRepository questionRepository;
+	
     @GetMapping("/")
     public String root() {
         return "index";
@@ -33,5 +44,39 @@ public class MainController {
         model.addObject("exam", exam);
         model.setViewName("add");
         return model;
+    }
+    @GetMapping(value = "/add")
+    public String addNewQuestion(@ModelAttribute Exam exam){
+    	List<QnOption> qnOptions = new ArrayList<>();
+        Question question = exam.getQuestion();
+        Answer answer = exam.getAnswer();
+        answer.setQuestion(question);
+        question.setAnswer(answer);
+        
+        QnOption option1 = new QnOption();
+        QnOption option2 = new QnOption();
+        QnOption option3 = new QnOption();
+        QnOption option4 = new QnOption();
+        
+       
+        option1.setQOption(exam.getOption1());
+        option2.setQOption(exam.getOption2());
+        option3.setQOption(exam.getOption3());
+        option4.setQOption(exam.getOption4());
+
+        option1.setQuestion(question);
+        option2.setQuestion(question);
+        option3.setQuestion(question);
+        option4.setQuestion(question);
+
+        qnOptions.add(option1);
+        qnOptions.add(option2);
+        qnOptions.add(option3);
+        qnOptions.add(option4);
+
+        question.setOptions(qnOptions);
+        
+        questionRepository.save(question);
+        return "index";
     }
 }
