c4fc0dd57045ed470c3104c4c77e541bba6ddc90
Ashikafs <ashik.hussain@lxisoft.com>
2020-10-16 20:51:02 +0530

    http://jira.bytatech.com:8080/browse/IN-11397

--- MovieScript/MovieScript/src/main/java/com/lxisoft/controller/MovieController.java
+++ MovieScript/MovieScript/src/main/java/com/lxisoft/controller/MovieController.java
@@ -6,17 +6,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 
 @Controller
 public class MovieController {
 
-    @GetMapping(value = {"/", "/home"})
+    System.out.();
+
+    @RequestMapping("/")
+    public String hello()
+    {
+        return "hello world Welcome";
+    }
+
+    @GetMapping(value ="/home")
     public String indexPage() {
         return "intro";
     }
