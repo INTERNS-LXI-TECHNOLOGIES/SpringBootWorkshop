fc106e8796154d5a4b62be6ff6a26923f87bccc0
ashikafs <ashik.hussain@lxisoft.com>
2021-01-07 18:41:03 +0530

    http://jira.bytatech.com:8080/browse/IN-12892

--- MovieScript/jhipster/MovieScript/src/main/java/com/lxisoft/web/rest/MovieControllerResource.java
+++ MovieScript/jhipster/MovieScript/src/main/java/com/lxisoft/web/rest/MovieControllerResource.java
@@ -49,16 +49,17 @@ public class MovieControllerResource {
         return "/login";
     }
 
-    @GetMapping("adminintro")
+    @GetMapping("/api/adminintro")
     public String showAdminPage()
     {
+       // System.out.println("Working");
         return "admin-intro";
     }
 
-    @GetMapping("userpg")
+    @GetMapping("/api/userpg")
     public String showUserPage()
     {
-        return "user";
+         return "user";
     }
 
     @GetMapping("newActor")
@@ -67,108 +68,108 @@ public class MovieControllerResource {
         model.addAttribute("movies", movieRepository.findAll());
         return "add-actor";
     }
-
-    @PostMapping("addActor")
-    public String addActor(@Valid Actor actor, BindingResult result, Model model) {
-        if (result.hasErrors()) {
-            return "add-actor";
-        }
-
-        actorRepository.save(actor);
-        return "redirect:adminintro";
-    }
-
-    @GetMapping("edit/{id}")
-    public String showUpdateForm(@PathVariable("id") long id, Model model) {
-        Actor actor = actorRepository.findById(id)
-            .orElseThrow(() -> new IllegalArgumentException("Invalid student Id:" + id));
-        model.addAttribute("actor", actor);
-        return "update-actors";
-    }
-
-    @PostMapping("update/{id}")
-    public String updateStudent(@PathVariable("id") long id, @Valid Actor actor, BindingResult result,
-                                Model model) {
-        if (result.hasErrors()) {
-            actor.setId(id);
-            return "update-student";
-        }
-
-        actorRepository.save(actor);
-        model.addAttribute("students", actorRepository.findAll());
-        return "index";
-    }
-
-    @GetMapping("newDialogue")
-    public String newDialogue(Dialogue dialogue,Model model)
-    {
-        model.addAttribute("actors", actorRepository.findAll());
-        return "add-dialogue";
-    }
-
-    @PostMapping("addDialogue")
-    public String addDialogue(@Valid Dialogue dialogue, BindingResult result, Model model) {
-        if (result.hasErrors()) {
-            return "add-dialogue";
-        }
-
-        dialogueRepository.save(dialogue);
-        return "redirect:adminintro";
-    }
-
-    @GetMapping("editdlg/{id}")
-    public String editDialogue(@PathVariable("id") long id, Model model) {
-        Dialogue dialogue = dialogueRepository.findById(id)
-            .orElseThrow(() -> new IllegalArgumentException("Invalid student Id:" + id));
-        model.addAttribute("dialogue", dialogue);
-        return "update-dialogue";
-    }
-
-    @PostMapping("updatedlg/{id}")
-    public String updateDialogue(@PathVariable("id") long id, @Valid Dialogue dialogue, BindingResult result,
-                                 Model model) {
-        if (result.hasErrors()) {
-            dialogue.setId(id);
-            return "update-dialogue";
-        }
-
-        dialogueRepository.save(dialogue);
-        return "adminintro";
-    }
-
-    @GetMapping("deletedlg/{id}")
-    public String deletDialogue(@PathVariable("id") long id, Model model) {
-        Dialogue dialogue = dialogueRepository.findById(id)
-            .orElseThrow(() -> new IllegalArgumentException("Invalid Actor Id:" + id));
-        dialogueRepository.delete(dialogue);
-
-        return "index";
-    }
-
-    @GetMapping("newMovie")
-    public String newMovie(Movie movie)
-    {
-        return "add-movie";
-    }
-
-    @PostMapping("addmovie")
-    public String addMovie(@Valid Movie movie, BindingResult result, Model model) {
-        if (result.hasErrors()) {
-            return "add-movie";
-        }
-
-        movieRepository.save(movie);
-        return "redirect:adminintro";
-    }
-
-
-    @GetMapping("playmovie")
-    public String moviePlay(Model model) {
-        model.addAttribute("actors", actorRepository.findAll());
-        model.addAttribute("dialogues", dialogueRepository.findAll());
-        model.addAttribute("movies", movieRepository.findAll());
-
-        return "movie-play";
-    }
+//
+//    @PostMapping("addActor")
+//    public String addActor(@Valid Actor actor, BindingResult result, Model model) {
+//        if (result.hasErrors()) {
+//            return "add-actor";
+//        }
+//
+//        actorRepository.save(actor);
+//        return "redirect:adminintro";
+//    }
+//
+//    @GetMapping("edit/{id}")
+//    public String showUpdateForm(@PathVariable("id") long id, Model model) {
+//        Actor actor = actorRepository.findById(id)
+//            .orElseThrow(() -> new IllegalArgumentException("Invalid student Id:" + id));
+//        model.addAttribute("actor", actor);
+//        return "update-actors";
+//    }
+//
+//    @PostMapping("update/{id}")
+//    public String updateStudent(@PathVariable("id") long id, @Valid Actor actor, BindingResult result,
+//                                Model model) {
+//        if (result.hasErrors()) {
+//            actor.setId(id);
+//            return "update-student";
+//        }
+//
+//        actorRepository.save(actor);
+//        model.addAttribute("students", actorRepository.findAll());
+//        return "index";
+//    }
+//
+//    @GetMapping("newDialogue")
+//    public String newDialogue(Dialogue dialogue,Model model)
+//    {
+//        model.addAttribute("actors", actorRepository.findAll());
+//        return "add-dialogue";
+//    }
+//
+//    @PostMapping("addDialogue")
+//    public String addDialogue(@Valid Dialogue dialogue, BindingResult result, Model model) {
+//        if (result.hasErrors()) {
+//            return "add-dialogue";
+//        }
+//
+//        dialogueRepository.save(dialogue);
+//        return "redirect:adminintro";
+//    }
+//
+//    @GetMapping("editdlg/{id}")
+//    public String editDialogue(@PathVariable("id") long id, Model model) {
+//        Dialogue dialogue = dialogueRepository.findById(id)
+//            .orElseThrow(() -> new IllegalArgumentException("Invalid student Id:" + id));
+//        model.addAttribute("dialogue", dialogue);
+//        return "update-dialogue";
+//    }
+//
+//    @PostMapping("updatedlg/{id}")
+//    public String updateDialogue(@PathVariable("id") long id, @Valid Dialogue dialogue, BindingResult result,
+//                                 Model model) {
+//        if (result.hasErrors()) {
+//            dialogue.setId(id);
+//            return "update-dialogue";
+//        }
+//
+//        dialogueRepository.save(dialogue);
+//        return "adminintro";
+//    }
+//
+//    @GetMapping("deletedlg/{id}")
+//    public String deletDialogue(@PathVariable("id") long id, Model model) {
+//        Dialogue dialogue = dialogueRepository.findById(id)
+//            .orElseThrow(() -> new IllegalArgumentException("Invalid Actor Id:" + id));
+//        dialogueRepository.delete(dialogue);
+//
+//        return "index";
+//    }
+//
+//    @GetMapping("newMovie")
+//    public String newMovie(Movie movie)
+//    {
+//        return "add-movie";
+//    }
+//
+//    @PostMapping("addmovie")
+//    public String addMovie(@Valid Movie movie, BindingResult result, Model model) {
+//        if (result.hasErrors()) {
+//            return "add-movie";
+//        }
+//
+//        movieRepository.save(movie);
+//        return "redirect:adminintro";
+//    }
+//
+//
+//    @GetMapping("playmovie")
+//    public String moviePlay(Model model) {
+//        model.addAttribute("actors", actorRepository.findAll());
+//        model.addAttribute("dialogues", dialogueRepository.findAll());
+//        model.addAttribute("movies", movieRepository.findAll());
+//
+//        return "movie-play";
+//    }
 
 }
