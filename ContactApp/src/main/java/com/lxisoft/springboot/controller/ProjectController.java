e18c667b10d3324938cbe164841d883b97445ba2
[SAHADH] <[sahadh.k.s@lxisoft.com]>
2022-07-05 18:29:13 +0530

    workingOnBoolean

--- ContactApp/src/main/java/com/lxisoft/springboot/controller/ProjectController.java
+++ ContactApp/src/main/java/com/lxisoft/springboot/controller/ProjectController.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ProjectController {
@@ -23,5 +25,15 @@ public class ProjectController {
         Project project = projectService.getProject(id);
         model.addAttribute("project", project);
         return "projectDetails";
+    }
+    @GetMapping("/createProject")
+    public String createProject(Model model) {
+        model.addAttribute("project", new Project());
+        return "addproject";
+    }
+    @PostMapping("/saveProject")
+    public String saveOwner(@ModelAttribute Project project) {
+        project = projectService.saveProject(project);
+        return "redirect:/project/" + project.getId();
     }
 }
