53125eb3d5df4e1ab3137f543d558d07dff0947c
kasujith <sujith.k.a@lxisoft.com>
2022-08-05 18:36:37 +0530

    Spring_Boot

--- Dictionary/src/main/java/com/lxisoft/dictionary/DictionaryApplication.java
+++ Dictionary/src/main/java/com/lxisoft/dictionary/DictionaryApplication.java
@@ -2,9 +2,7 @@ package com.lxisoft.dictionary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan({"com.lxisoft.dictionary.controller"})
 @SpringBootApplication
 public class DictionaryApplication {
 
