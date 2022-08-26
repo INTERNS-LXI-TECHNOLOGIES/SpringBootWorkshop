5ebedcac16590ce0e699e06cccb3fb32875a468c
kasujith <sujith.k.a@lxisoft.com>
2022-08-26 17:40:55 +0530

    CSV

--- Dictionary/src/main/java/com/lxisoft/dictionary/controller/DictionaryController.java
+++ Dictionary/src/main/java/com/lxisoft/dictionary/controller/DictionaryController.java
@@ -61,6 +61,4 @@ public class DictionaryController {
     public String login() {
         return "Login-form";
     }
-
-
 }
---
+++ Dictionary/src/test/java/com/lxisoft/dictionary/DictionaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lxisoft.dictionary;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DictionaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
